14e5a0249e9410f92d1380c62cc3c0dd4f4207a7
Stefan Stojkovic <skyrim@live.com>
2017-01-11 13:32:10 +0100

    Implemented basic replay playing functionality
    https://trello.com/c/tw6TIba7

--- src/replay-player.js
+++ src/replay-player.js
@@ -1,40 +1,47 @@
+import * as Time from './time'
+
 export default class ReplayPlayer {
     constructor(data) {
         this.frames = data.frames
-        this.beginningTime = data.beginningTime
-        this.timeLength = data.timeLength
-        this.currentTime = 0
+        this.beginningTime = data.meta.beginningTime
+        this.timeLength = data.meta.timeLength
+        this.currentTime = data.frames[0].time
         this.currentFrame = 0
+
+        this.isPlaying = false
+        this.isPaused = false
+        this.startTime = 0
+        this.pauseTime = 0
     }
 
     getFrameByTime(time) {
         if (time > this.currentTime) {
             for (let i = this.currentFrame; i < this.frames.length; ++i) {
-                if (this.frames.time[i] > time) {
+                if (this.frames[i].time > time) {
                     this.currentTime = time
                     this.currentFrame = i
-                    let left = Math.ceil(0, i - 1)
+                    let left = Math.max(0, i - 1)
                     let right = i
 
                     return {
-                        position: this.frames.position[left], // TODO: interp
-                        rotation: this.frames.rotation[left], // TODO: interp
-                        command: this.frames.command[left]
+                        position: this.frames[left].position, // TODO: interp
+                        rotation: this.frames[left].rotation, // TODO: interp
+                        command: this.frames[left].commands
                     }
                 }
             }
         } else {
             for (let i = this.currentFrame; i >= 0; --i) {
-                if (this.frames.time[i] < time) {
+                if (this.frames[i].time < time) {
                     this.currentTime = time
                     this.currentFrame = i
-                    let left = Math.ceil(0, i - 1)
+                    let left = Math.max(0, i - 1)
                     let right = i
 
                     return {
-                        position: this.frames.position[left], // TODO: interp
-                        rotation: this.frames.rotation[left], // TODO: interp
-                        command: this.frames.command[left]
+                        position: this.frames[left].position, // TODO: interp
+                        rotation: this.frames[left].rotation, // TODO: interp
+                        command: this.frames[left].commands
                     }
                 }
             }
@@ -43,37 +50,80 @@ export default class ReplayPlayer {
         return null
     }
 
-    static createFromReplay(replay) {
-        // Using structure of arrays instead of array of structures
-        // because command are present in few frames
-        // and I could save some memory by using sparse array
-        // (arrays automatically become sparse when they have many undefined elements)
-        // Also, frame lookup by time should be faster (cache hits).
-        let frames = {
-            time:     [],
-            position: [],
-            rotation: [],
-            command:  [] // <- sparse array
+    play() {
+        let currentTime = Time.now() / 1000
+        if (this.isPlaying) {
+            if (this.isPaused) {
+                this.isPaused = false
+                this.startTime += currentTime - this.pauseTime
+            } else {
+                this.isPaused = true
+                this.pauseTime = currentTime
+            }
+        } else {
+            this.isPlaying = true
+            this.isPaused = false
+            this.startTime = currentTime
+        }
+    }
+
+    stop() {
+        this.isPlaying = false
+        this.isPaused = false
+        this.startTime = 0
+        this.pauseTime = 0
+    }
+
+    getFrame() {
+        let currentTime = this.isPaused
+            ? this.pauseTime
+            : Time.now() / 1000
+        let dt = currentTime - this.startTime
+        let frame = this.getFrameByTime(this.beginningTime + dt)
+        if (!frame) {
+            this.stop()
         }
 
+        return frame
+    }
+
+    static createFromReplay(replay) {
+        let frames = []
+
         for (let i = 0; i < replay.directories.length; ++i) {
-            let macros = replay.directories[i].macros
-            for (let j = 0; j < macros.length; ++i) {
-                let macro = macros[i]
-                if (macro.type === 0 || macro.type === 1) {
-                    frames.time.push(macro.time)
-                    frames.position.push(macro.camera.position)
-                    frames.position.push(macro.camera.orientation)
-                } else if (macro.type === 3) {
-                    frames.command[frames.position.length - 1] = macro.command
-                }
+            if (replay.directories[i].name !== 'Playback') {
+                continue
             }
+
+            let macros = replay.directories[i].macros
+            macros.forEach(macro => {
+                let frame
+                if (frames.length && frames[frames.length - 1].id === macro.frame) {
+                    frame = frames[frames.length - 1]
+                } else {
+                    frame = {
+                        id: macro.frame,
+                        time: macro.time,
+                        position: null,
+                        rotation: null,
+                        commands: []
+                    }
+                    frames.push(frame)
+                }
+
+                if (macro.type === 0 || macro.type === 1) {
+                    frame.position = macro.camera.position
+                    frame.rotation = macro.camera.orientation
+                } else if (macro.type === 3) {
+                    frame.commands.push(macro.command)
+                }
+            })
         }
 
         return new ReplayPlayer({
             meta: {
-                beginningTime: replay.directories[0].macros[0].time,
-                timeLength: replay.directories[replay.directories.length - 1].macros.slice(-1)[0].time
+                beginningTime: frames[0].time,
+                timeLength: frames[frames.length - 1].time
             },
             frames
         })

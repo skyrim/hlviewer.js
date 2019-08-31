6f5cef42795a4980c8beb853d642c459cff1d52c
Stefan Stojkovic <skyrim@live.com>
2019-08-31 08:20:33 +0200

    added uncomitted config file

---
+++ src/Config.ts
@@ -0,0 +1,61 @@
+type ConfigPaths = {
+  base: string
+  replays: string
+  maps: string
+  wads: string
+  skies: string
+  sounds: string
+}
+
+export class Config {
+  public static init(params: any): Config {
+    if (typeof params === 'string') {
+      return new Config({
+        paths: {
+          base: params,
+          replays: `${params}/replays`,
+          maps: `${params}/maps`,
+          wads: `${params}/wads`,
+          skies: `${params}/skies`,
+          sounds: `${params}/sounds`
+        }
+      })
+    } else {
+      return new Config({
+        paths: {
+          base: (params && params.paths && params.paths.base) || '',
+          replays: (params && params.paths && params.paths.replays) || '/replays',
+          maps: (params && params.paths && params.paths.maps) || '/maps',
+          wads: (params && params.paths && params.paths.wads) || '/wads',
+          skies: (params && params.paths && params.paths.skies) || '/skies',
+          sounds: (params && params.paths && params.paths.sounds) || '/sounds'
+        }
+      })
+    }
+  }
+
+  private paths: ConfigPaths
+
+  constructor(params: { paths: ConfigPaths }) {
+    this.paths = { ...params.paths }
+  }
+
+  getBasePath() {
+    return this.paths.base
+  }
+  getReplaysPath() {
+    return this.paths.replays
+  }
+  getMapsPath() {
+    return this.paths.maps
+  }
+  getWadsPath() {
+    return this.paths.wads
+  }
+  getSkiesPath() {
+    return this.paths.skies
+  }
+  getSoundsPath() {
+    return this.paths.sounds
+  }
+}

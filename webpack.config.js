c08c58a365c25c9d1b58d4affad0bfdf2a15d8e4
Stefan Stojković <skyrim@live.com>
2019-09-17 19:56:01 +0200

    replaced babel minifier with terser

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,12 @@
 const fs = require('fs')
 const path = require('path')
 const webpack = require('webpack')
-const MinifyPlugin = require('babel-minify-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin
 
 const license = fs.readFileSync('LICENSE', 'utf8')
 
 const isProduction = process.env.NODE_ENV === 'production'
-
-const plugins = []
-if (isProduction) {
-  plugins.push(new MinifyPlugin())
-}
-plugins.push(
-  new CheckerPlugin(),
-  new webpack.BannerPlugin({
-    banner: license
-  }),
-  new webpack.DefinePlugin({
-    VERSION: JSON.stringify(require('./package.json').version)
-  })
-)
 
 module.exports = {
   mode: isProduction ? 'production' : 'development',
@@ -44,8 +30,22 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.ts', '.tsx', '.jsx']
   },
-  plugins,
+  plugins: [
+    new CheckerPlugin(),
+    new webpack.BannerPlugin({
+      banner: license
+    }),
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify(require('./package.json').version)
+    })
+  ],
   node: {
     fs: 'empty'
-  }
+  },
+  optimization: isProduction
+    ? {
+        minimize: true,
+        minimizer: [new TerserPlugin()]
+      }
+    : undefined
 }

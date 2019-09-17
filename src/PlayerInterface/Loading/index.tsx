b5ef84b645246c312ed9e93de8e3f94a03e0d401
Stefan Stojković <skyrim@live.com>
2019-09-17 07:56:38 +0200

    refactored loading components

--- src/PlayerInterface/Loading/index.tsx
+++ src/PlayerInterface/Loading/index.tsx
@@ -82,53 +82,61 @@ export class Loading extends Component<LoadingProps, LoadingState> {
     this.forceUpdate()
   }
 
-  formatItem(name: string, progress: number) {
-    name = itemTypeGroupName[name]
-    const status = Math.round(progress * 100) + '%'
-
-    let length = 29 - name.length - status.length
-    if (length < 2) {
-      length = 9 - status.length
-    }
-
-    const dots = Array(length).join('.')
-
-    return `${name}${dots}${status}`
-  }
-
   render() {
     return (
       <div class={this.props.visible ? s.loading : s.loadingHidden}>
-        <div class={s.spinner}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="80px"
-            height="80px"
-            viewBox="0 0 80 80"
-            xmlSpace="preserve"
-          >
-            <path
-              fill="#ffffff"
-              width="10px"
-              d="M40,72C22.4,72,8,57.6,8,40C8,22.4,22.4,8,40,8c17.6,0,32,14.4,32,32c0,1.1-0.9,2-2,2s-2-0.9-2-2c0-15.4-12.6-28-28-28S12,24.6,12,40s12.6,28,28,28c1.1,0,2,0.9,2,2S41.1,72,40,72z"
-            />
-          </svg>
-        </div>
+        <Spinner />
 
         <ul class={s.log}>
           {Object.entries(this.state.items).map(([name, items]) => (
-            <li key={name} class={s.logItem}>
-              {this.formatItem(
-                name,
+            <LoadingItem
+              name={name}
+              progress={
                 items.reduce((prev, cur) => prev + cur.progress, 0) /
-                  items.length
-              )}
-            </li>
+                items.length
+              }
+            />
           ))}
         </ul>
       </div>
     )
   }
+}
+
+function Spinner() {
+  return (
+    <div class={s.spinner}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="80px"
+        height="80px"
+        viewBox="0 0 80 80"
+        xmlSpace="preserve"
+      >
+        <path
+          fill="#ffffff"
+          width="10px"
+          d="M40,72C22.4,72,8,57.6,8,40C8,22.4,22.4,8,40,8c17.6,0,32,14.4,32,32c0,1.1-0.9,2-2,2s-2-0.9-2-2c0-15.4-12.6-28-28-28S12,24.6,12,40s12.6,28,28,28c1.1,0,2,0.9,2,2S41.1,72,40,72z"
+        />
+      </svg>
+    </div>
+  )
+}
+
+function LoadingItem(props: { name: string; progress: number }) {
+  const name = itemTypeGroupName[props.name]
+  const status = Math.round(props.progress * 100) + '%'
+
+  let length = 29 - name.length - status.length
+  if (length < 2) {
+    length = 9 - status.length
+  }
+
+  const dots = Array(length).join('.')
+
+  const fmt = `${name}${dots}${status}`
+
+  return <li class={s.logItem}>{fmt}</li>
 }
